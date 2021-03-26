a1e4ae5a301ce9ade39468fddd2233d5c2965be5
LucPrestin <l.prestin@gmx.de>
2021-03-26 22:16:42 +0100

    reduce O(n**3) problem to O(n) problem

--- level/level.gd
+++ level/level.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const tile_pixel_factor = 16
+const PIXEL_PER_TILE = 16
 const SURROUNDING_TILE_RANGE = 2
 
 # Called when the node enters the scene tree for the first time.
@@ -14,15 +14,10 @@ func _ready():
 func get_surrounding_trees(position: Vector2):
 	var surrounding_trees = []
 	
-	var grid_position = (position / tile_pixel_factor).round()
-	var forest = get_node("trees").get_children()
-	
-	for offset_x in range(SURROUNDING_TILE_RANGE * -1, SURROUNDING_TILE_RANGE):
-		for offset_y in range(SURROUNDING_TILE_RANGE * -1, SURROUNDING_TILE_RANGE):
-			for tree in forest:
-				var tree_grid_position = (tree.get_position() / tile_pixel_factor).round()
-				if tree_grid_position == grid_position + Vector2(offset_x, offset_y):
-					surrounding_trees.append(tree)
+	for tree in $trees.get_children():
+		var tree_position = tree.get_position() / PIXEL_PER_TILE
+		if position.distance_to(tree_position) <= SURROUNDING_TILE_RANGE:
+			surrounding_trees.append(tree)
 	
 	return surrounding_trees
 
