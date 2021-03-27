702d4aee9d41a2c397cad5bbf49d9430b0d3edac
LucPrestin <l.prestin@gmx.de>
2021-03-27 17:31:52 +0100

    add burned flower textures

--- flora/flower_information.gd
+++ flora/flower_information.gd
@@ -6,5 +6,5 @@ static func get_healthy_texture():
 	return "res://resources/flora/flower_%s.png" % texture_id
 
 static func get_burnt_texture():
-	var texture_id = randi() % 5 + 1
-	return "res://resources/flora/tree_burnt_%s.png" % texture_id
+	var texture_id = randi() % 12 + 1
+	return "res://resources/flora/flower_burnt_%s.png" % texture_id
